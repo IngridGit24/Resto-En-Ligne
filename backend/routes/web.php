34f9ec5b07afe434dd41436949a2ab222c970ee1
IngridGit24 <moudoungouingrid@gmail.com>
2025-04-14 13:56:48 +0100

    upadated

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,15 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Manage Restaurant routes
-Route::get('/menus', [RestoManager::class, 'index']);
-Route::post('/menus', [RestoManager::class, 'store']);
-Route::get('/menus/{id}', [RestoManager::class, 'show']);
-Route::put('/menus/{id}', [RestoManager::class, 'update']);
-Route::delete('/menus/{id}', [RestoManager::class, 'destroy']);
+Route::prefix('api')->group(function () {
+    //Manage Restaurant routes
+    Route::get('/menus', [RestoManager::class, 'index']);
+    Route::post('/menus', [RestoManager::class, 'store']);
+    Route::get('/menus/{id}', [RestoManager::class, 'show']);
+    Route::put('/menus/{id}', [RestoManager::class, 'update']);
+    Route::delete('/menus/{id}', [RestoManager::class, 'destroy']);
 
-Route::get('/restaurants', [RestoManager::class, 'indexRestaurants']);
-Route::post('/restaurants', [RestoManager::class, 'storeRestaurant']);
-Route::get('/restaurants/{id}', [RestoManager::class, 'showRestaurant']);
-Route::put('/restaurants/{id}', [RestoManager::class, 'updateRestaurant']);
-Route::delete('/restaurants/{id}', [RestoManager::class, 'destroyRestaurant']);
+    Route::get('/restaurants', [RestoManager::class, 'indexRestaurants']);
+    Route::post('/restaurants', [RestoManager::class, 'storeRestaurant']);
+    Route::get('/restaurants/{id}', [RestoManager::class, 'showRestaurant']);
+    Route::put('/restaurants/{id}', [RestoManager::class, 'updateRestaurant']);
+    Route::delete('/restaurants/{id}', [RestoManager::class, 'destroyRestaurant']);
+});
